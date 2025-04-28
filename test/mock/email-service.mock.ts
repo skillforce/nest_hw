dd9c4ce1280d4c74f3cd6487d43fd03f9a49638d
denis_tatarinov <skillforce@mail.ru>
2025-04-28 13:10:01 +0300

    hw17 start

--- test/mock/email-service.mock.ts
+++ test/mock/email-service.mock.ts
@@ -4,6 +4,8 @@ export class EmailServiceMock extends EmailService {
   //override method
   async sendConfirmationEmail(email: string, code: string): Promise<void> {
     console.log('Call mock method sendConfirmationEmail / EmailServiceMock');
+    console.log(email);
+    console.log(code);
 
     return;
   }
