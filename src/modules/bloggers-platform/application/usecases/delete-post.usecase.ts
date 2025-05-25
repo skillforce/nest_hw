53ade43dce1cd38f4d76b644ddfeec1e90c2af05
denis_tatarinov <skillforce@mail.ru>
2025-05-25 19:39:30 +0300

    hw20

--- src/modules/bloggers-platform/application/usecases/delete-post.usecase.ts
+++ src/modules/bloggers-platform/application/usecases/delete-post.usecase.ts
@@ -1,8 +1,6 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { PostsRepository } from '../../infrastructure/posts.repository';
 import { Post } from '../../domain/post.entity';
-import { DomainException } from '../../../../core/exceptions/domain-exceptions';
-import { DomainExceptionCode } from '../../../../core/exceptions/domain-exception-codes';
 
 export class DeletePostCommand {
   constructor(public id: number) {}
@@ -15,19 +13,6 @@ export class DeletePostUseCase
   constructor(private postsRepository: PostsRepository) {}
 
   async execute({ id }: DeletePostCommand): Promise<void> {
-    if (isNaN(id)) {
-      throw new DomainException({
-        code: DomainExceptionCode.NotFound,
-        extensions: [
-          {
-            field: 'post',
-            message: 'post not found',
-          },
-        ],
-        message: 'post not found',
-      });
-    }
-
     const post = await this.postsRepository.findOrNotFoundFail(id);
 
     const deletedPost = this.makeDeleted(post);
